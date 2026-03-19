312ba102410e63e1aab845725f70909a42d38413
PreethiKannan38 <preethikannan0805@gmail.com>
2026-03-19 21:10:00 +0530

    merge conflit resolved

--- frontend/script.js
+++ frontend/script.js
@@ -60,8 +60,6 @@ const contentData = {
             <div class="dashboard-container">
                 <div class='tableauPlaceholder' id='viz1773927011080' style='position: relative'><noscript><a href='#'><img alt='Reddit Dash ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;Reddit_Dashboard&#47;RedditDash&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Reddit_Dashboard&#47;RedditDash' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;Reddit_Dashboard&#47;RedditDash&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1773927011080');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1827px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
             </div>
-<<<<<<< Updated upstream
-=======
             <div class="wiki-box insights-section">
                 <h2>Data Insights & Analysis</h2>
                 <div class="insight-item">
@@ -85,7 +83,6 @@ const contentData = {
                     <p>This was done just to test how VADER and ROBERTA models worked and how they differed from each other.</p>
                 </div>
             </div>
->>>>>>> Stashed changes
             <div class="footer">Interactive dashboard powered by Tableau Public</div>
         `,
         callback: () => {
@@ -98,9 +95,6 @@ const contentData = {
             vizElement.parentNode.insertBefore(scriptElement, vizElement);
         }
     },
-<<<<<<< Updated upstream
-    
-=======
     "Editor analysis": {
         title: "Wikipedia Editor Analysis",
         subtitle: "Interactive Tableau Dashboard for regional and platform-level insights",
@@ -120,7 +114,6 @@ const contentData = {
             vizElement.parentNode.insertBefore(scriptElement, vizElement);
         }
     },
->>>>>>> Stashed changes
 };
 
 function renderContent(key) {
